e855e3868be5f38feb0f78eeeaa0425378558acc
bun919tw <bun919tw@gmail.com>
2016-03-27 18:59:45 +0800

    Add responsive web design prototype

--- web/static/web/js/index.js
+++ web/static/web/js/index.js
@@ -1,32 +1,34 @@
 window.onload = function() {
   var init_height = $(window).scrollTop();
-  if (init_height < 800) {
-    $("#section1_brick").addClass("brick");
-    $("#section2_brick").removeClass("brick");
-    $("#section3_brick").removeClass("brick");
-  } else if (init_height >= 800 && init_height < 1600) {
-    $("#section1_brick").removeClass("brick");
-    $("#section2_brick").addClass("brick");
-    $("#section3_brick").removeClass("brick");
-  } else if (init_height >= 1600) {
-    $("#section1_brick").removeClass("brick");
-    $("#section2_brick").removeClass("brick");
-    $("#section3_brick").addClass("brick");
+  var section2_anchor = $(".section2").position().top;
+  var section3_anchor = $(".section3").position().top;
+  if (init_height < section2_anchor) {
+    $("#section1_brick").addClass("show");
+    $("#section2_brick").removeClass("show");
+    $("#section3_brick").removeClass("show");
+  } else if (init_height >= section2_anchor && init_height < section3_anchor) {
+    $("#section1_brick").removeClass("show");
+    $("#section2_brick").addClass("show");
+    $("#section3_brick").removeClass("show");
+  } else if (init_height >= section3_anchor) {
+    $("#section1_brick").removeClass("show");
+    $("#section2_brick").removeClass("show");
+    $("#section3_brick").addClass("show");
   }
   window.onscroll = function() {
     var height = $(window).scrollTop();
-    if (height < 800) {
-      $("#section1_brick").addClass("brick");
-      $("#section2_brick").removeClass("brick");
-      $("#section3_brick").removeClass("brick");
-    } else if (height >= 800 && height < 1600) {
-      $("#section1_brick").removeClass("brick");
-      $("#section2_brick").addClass("brick");
-      $("#section3_brick").removeClass("brick");
-    } else if (height >= 1600) {
-      $("#section1_brick").removeClass("brick");
-      $("#section2_brick").removeClass("brick");
-      $("#section3_brick").addClass("brick");
+    if (height < section2_anchor) {
+      $("#section1_brick").addClass("show");
+      $("#section2_brick").removeClass("show");
+      $("#section3_brick").removeClass("show");
+    } else if (height >= section2_anchor && height < section3_anchor) {
+      $("#section1_brick").removeClass("show");
+      $("#section2_brick").addClass("show");
+      $("#section3_brick").removeClass("show");
+    } else if (height >= section3_anchor) {
+      $("#section1_brick").removeClass("show");
+      $("#section2_brick").removeClass("show");
+      $("#section3_brick").addClass("show");
     }
   }
 }
